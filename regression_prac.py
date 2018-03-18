473bf44ffa7e1fc782855bde9c4f3c2eb4566631
YiChenWuJane <lc007274@gmail.com>
2018-03-18 19:55:05 +0800

    In this version, the training with different epoch number is demostrated.

--- regression_prac.py
+++ regression_prac.py
@@ -10,26 +10,29 @@ from keras.models import Sequential
 from keras.layers import Dense
 import matplotlib.pyplot as plt
 
-X = np.linspace(-1, 1, 200)
+N = 200;
+X = np.linspace(-1, 1, N)
 np.random.shuffle(X)
-Y = 3*X + 0.5 +  np.random.normal(0, 0.4, (200,))
+Y = 3*X + 0.5 +  np.random.normal(0, 0.4, (N,))
 
 X_train, Y_train = X[:60], Y[:60]
 X_test, Y_test = X[60:], Y[60:]
 
-plt.scatter(X, Y)
-plt.show()
+#plt.scatter(X, Y)
+#plt.show()
 
 model = Sequential()
 model.add(Dense(output_dim=1, input_dim = 1))
 model.compile(loss='mse', optimizer='sgd')
 
-K = model.fit(X_train, Y_train, batch_size=1, epochs=50)
-print(K.history)
-W, b = model.layers[0].get_weights();
-print('Weight=', W, ', bias=', b)
 
-Y_pred = model.predict(X_test)
-plt.scatter(X_test, Y_test)
-plt.plot(X_test, Y_pred)
-plt.show()
+for k in np.arange(1, 51): 
+ Hist = model.fit(X_train, Y_train, batch_size=N, epochs=k, verbose=0)
+#print(K.history)
+ W, b = model.layers[0].get_weights()
+ print('For epoch =', k, 'Weight=', W, ', bias=', b)
+
+ Y_pred = model.predict(X_test)
+ plt.scatter(X_test, Y_test)
+ plt.plot(X_test, Y_pred)
+ plt.show()
